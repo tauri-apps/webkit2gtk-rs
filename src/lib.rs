da2677c1d6582f5abeb1ee0e65a97c8fccd8d062
Antoni Boucher <bouanto@zoho.com>
2016-09-14 10:53:31 -0400

    Initial version.

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+#![allow(unused_imports)]
+
+#[macro_use]
+extern crate glib;
+extern crate glib_sys as glib_ffi;
+extern crate gobject_sys as gobject_ffi;
+extern crate gtk;
+extern crate gtk_sys;
+extern crate libc;
+
+extern crate webkit2_sys as ffi;
+
+macro_rules! callback_guard {
+    () => (
+        let _guard = ::glib::CallbackGuard::new();
+    )
+}
+
+mod auto;
+
+pub use auto::*;
