d54ab14b10785d78cd61463b8f9052e295aeb16d
Antoni Boucher <bouanto@zoho.com>
2019-04-13 18:53:36 -0400

    Regen

--- src/web_view.rs
+++ src/web_view.rs
@@ -26,13 +26,13 @@ use std::ptr;
 
 use glib::IsA;
 #[cfg(feature = "v2_6")]
-use glib::object::Downcast;
+use glib::object::Cast;
 #[cfg(feature = "v2_6")]
 use glib::StaticType;
 #[cfg(feature = "v2_6")]
 use glib::translate::{FromGlibPtrNone, ToGlib, ToGlibPtr};
 #[cfg(feature = "v2_6")]
-use gobject_ffi;
+use gobject_sys;
 use gtk;
 
 #[cfg(feature = "v2_6")]
@@ -54,11 +54,13 @@ impl<O> WebViewExtManual for O
         assert_initialized_main_thread!();
         let user_content_manager_property = CString::new("user-content-manager").unwrap();
         let web_context_property = CString::new("web-context").unwrap();
-        let glib_user_content_manager: *mut gobject_ffi::GObject = user_content_manager.to_glib_none().0;
-        let glib_context: *mut gobject_ffi::GObject = context.to_glib_none().0;
-        let null: *mut gobject_ffi::GObject = ptr::null_mut();
+        let glib_user_content_manager: *mut webkit2_sys::WebKitUserContentManager = user_content_manager.to_glib_none().0;
+        let glib_user_content_manager = glib_user_content_manager as *mut gobject_sys::GObject;
+        let glib_context: *mut webkit2_sys::WebKitWebContext = context.to_glib_none().0;
+        let glib_context = glib_context as *mut gobject_sys::GObject;
+        let null: *mut gobject_sys::GObject = ptr::null_mut();
         unsafe {
-            gtk::Widget::from_glib_none(gobject_ffi::g_object_new(WebView::static_type().to_glib(), user_content_manager_property.as_ptr(), glib_user_content_manager, web_context_property.as_ptr(), glib_context, null) as *mut _).downcast_unchecked()
+            gtk::Widget::from_glib_none(gobject_sys::g_object_new(WebView::static_type().to_glib(), user_content_manager_property.as_ptr(), glib_user_content_manager, web_context_property.as_ptr(), glib_context, null) as *mut _).downcast().expect("downcast")
         }
     }
 }
